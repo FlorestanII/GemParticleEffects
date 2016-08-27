f4d8ede05eb2836eeaad688321b6b550f4affc60
Johannes <Johannes@Johannes-Laptop>
2016-08-27 08:17:19 +0200

    Intial commit.

---
+++ src/main/java/me/florestanii/gemparticles/GemParticleEffects.java
@@ -0,0 +1,22 @@
+package me.florestanii.gemparticles;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class GemParticleEffects extends JavaPlugin{
+
+	@Override
+	public void onLoad() {
+		super.onLoad();
+	}
+	
+	@Override
+	public void onEnable() {
+		super.onEnable();
+	}
+	
+	@Override
+	public void onDisable() {
+		super.onDisable();
+	}
+	
+}
