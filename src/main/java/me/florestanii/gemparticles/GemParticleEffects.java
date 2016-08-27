962db39a674ea8efc050ce095907a29441b4bef5
Johannes <Johannes@Johannes-Laptop>
2016-08-27 09:18:26 +0200

    Add main implementation for the player data storage.

--- src/main/java/me/florestanii/gemparticles/GemParticleEffects.java
+++ src/main/java/me/florestanii/gemparticles/GemParticleEffects.java
@@ -1,13 +1,19 @@
 package me.florestanii.gemparticles;
 
+import java.util.UUID;
+
+import org.bukkit.OfflinePlayer;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import de.craften.plugins.mcguilib.ViewManager;
+import de.craften.plugins.playerdatastore.api.PlayerDataStore;
+import de.craften.plugins.playerdatastore.api.PlayerDataStoreService;
 
 public class GemParticleEffects extends JavaPlugin{
 
 	private static GemParticleEffects instance;
 	private ViewManager viewManager;
+	private PlayerDataStoreService storage;
 	
 	@Override
 	public void onLoad() {
@@ -19,10 +25,12 @@ public class GemParticleEffects extends JavaPlugin{
 	
 	@Override
 	public void onEnable() {
-		
+
 		getCommand("particles").setExecutor(new ParticlesCommand());
 		
 		this.viewManager = new ViewManager(this);
+		
+		this.storage = getServer().getServicesManager().getRegistration(PlayerDataStoreService.class).getProvider();
 		
 		super.onEnable();
 	}
@@ -40,4 +48,15 @@ public class GemParticleEffects extends JavaPlugin{
 		return viewManager;
 	}
 	
+	public PlayerDataStoreService getStorage() {
+		return storage;
+	}
+	
+	public PlayerDataStore getPlayerStorage(OfflinePlayer player) {
+		return storage.getStore(player);
+	}
+	
+	public PlayerDataStore getPlayerStorage(UUID uuid) {
+		return storage.getStore(uuid);
+	}
 }
