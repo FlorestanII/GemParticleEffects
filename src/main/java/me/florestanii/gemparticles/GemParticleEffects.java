43a2bbcc712295b36df32e01acacf4a7955dfcc5
Johannes <Johannes@Johannes-Laptop>
2016-08-27 09:03:19 +0200

    Add command to open the particle menu.

--- src/main/java/me/florestanii/gemparticles/GemParticleEffects.java
+++ src/main/java/me/florestanii/gemparticles/GemParticleEffects.java
@@ -2,21 +2,42 @@ package me.florestanii.gemparticles;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import de.craften.plugins.mcguilib.ViewManager;
+
 public class GemParticleEffects extends JavaPlugin{
 
+	private static GemParticleEffects instance;
+	private ViewManager viewManager;
+	
 	@Override
 	public void onLoad() {
+		
+		instance = this;
+		
 		super.onLoad();
 	}
 	
 	@Override
 	public void onEnable() {
+		
+		getCommand("particles").setExecutor(new ParticlesCommand());
+		
+		this.viewManager = new ViewManager(this);
+		
 		super.onEnable();
 	}
 	
 	@Override
 	public void onDisable() {
 		super.onDisable();
+	}
+	
+	public static GemParticleEffects getPlugin() {
+		return instance;
+	}
+	
+	public ViewManager getViewManager() {
+		return viewManager;
 	}
 	
 }
---
+++ src/main/java/me/florestanii/gemparticles/ParticleMenu.java
@@ -0,0 +1,11 @@
+package me.florestanii.gemparticles;
+
+import de.craften.plugins.mcguilib.SinglePageView;
+
+public class ParticleMenu extends SinglePageView{
+
+	public ParticleMenu() {
+		super("§6§l★ §4§lParticle Menu §6§l ★", 54);
+	}
+	
+}
